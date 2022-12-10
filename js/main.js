0ce5ad6050cb9ccba5f62a4a516ce80255fe3a17
radjanio <117517777+radjanio@users.noreply.github.com>
2022-12-10 15:58:40 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src =  "imagens/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('icon').src =  "imagens/menu-close.png";  
+    }
+}
